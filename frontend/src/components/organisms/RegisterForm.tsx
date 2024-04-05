eeeb2764200ce7302d741358e9606aff86b08f0b
Battino <battino46@gmail.com>
2024-04-05 12:01:51 +0200

    WIP add register page

---
+++ frontend/src/components/organisms/RegisterForm.tsx
@@ -0,0 +1,45 @@
+import { Form, Formik } from "formik";
+import React, { useState } from "react";
+import LoginInput from "../molecules/LoginInput";
+import FormTwoButtons from "../atoms/FormTwoButtons";
+import { registerUser } from "../../services/UserService";
+import { AxiosError } from "axios";
+import { UserLogin } from "../../model";
+import { useNavigate } from "react-router-dom";
+
+function RegisterForm() {
+  const nav = useNavigate();
+  const [failStatus, setFailStatus] = useState<null | string>(null);
+  const handleSubmit = (values: UserLogin) => {
+    registerUser(values)
+      .then(() => {
+        setFailStatus(null);
+        nav("/login");
+      })
+      .catch((e: AxiosError) => {
+        String(e.response?.data);
+      });
+  };
+  return (
+    <div className="w-full max-w-xs">
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <LoginInput failStatus={failStatus} />
+          <FormTwoButtons
+            linkButtonURL="/register"
+            linkButtonText="Register"
+            submitButtonText="Sign In"
+          />
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default RegisterForm;
---
+++ frontend/src/components/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RegisterPage() {
+  return (
+    <div className="h-screen bg-gray-200 flex justify-center items-center">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default RegisterPage;

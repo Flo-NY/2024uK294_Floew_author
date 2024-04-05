bf0b34bfde2a8b106f041658b9186008a7bdd596
Battino <battino46@gmail.com>
2024-04-05 12:02:21 +0200

    add pagination to AuthorPage

--- frontend/src/components/pages/AuthorsPage.tsx
+++ frontend/src/components/pages/AuthorsPage.tsx
@@ -1,21 +1,37 @@
-import { Button } from "@mui/material";
-import React, { useEffect } from "react";
-import { Author } from "../../services/AuthorService";
-import { tokenValid as isTokenValid } from "../../services/UserService";
+import { Button, Pagination } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Author, getAuthorPage } from "../../services/AuthorService";
+import { tokenValid } from "../../services/UserService";
 import { useNavigate } from "react-router-dom";
 
 function AuthorsPage() {
-  const navigator = useNavigate();
-  useEffect(() => {
-    isTokenValid().then((tokenValid) => {
-      console.log(tokenValid);
-      if (!tokenValid) {
-        navigator("login");
-        console.log(tokenValid);
-      }
-    });
-  });
-  return <div></div>;
+  const nav = useNavigate();
+  const [authorList, setAuthorList] = useState<Author[]>([]);
+  const [page, setPage] = useState<number>(1);
+
+  function requestAuthorList() {
+    getAuthorPage(page)
+      .then(setAuthorList)
+      .catch((error) => {
+        if (error.response && error.response.status === 401) {
+          console.log("navigating");
+          nav("/login");
+        } else {
+          console.error(error);
+        }
+      });
+  }
+  useEffect(requestAuthorList), [page];
+  return (
+    <>
+      <div>{JSON.stringify(authorList)}</div>;
+      <Pagination
+        count={10}
+        page={page}
+        onChange={(_, value) => setPage(value)}
+      />
+    </>
+  );
 }
 
 export default AuthorsPage;
